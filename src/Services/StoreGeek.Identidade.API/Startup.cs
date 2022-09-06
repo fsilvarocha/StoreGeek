d04e1b3a5d74806bc0d8bf8fe02a274cae2c3155
Fabricio Silva da Rocha <fsilvarocha@outlook.com>
2022-09-05 21:36:47 -0300

    Recriando API de Identity

--- src/Services/StoreGeek.Identidade.API/Startup.cs
+++ src/Services/StoreGeek.Identidade.API/Startup.cs
@@ -1,15 +1,15 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-using StoreGeek.Identidade.API.Data;
-using StoreGeek.Identidade.API.Extensions;
-using System.Text;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace StoreGeek.Identidade.API
 {
@@ -22,47 +22,13 @@ namespace StoreGeek.Identidade.API
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddDefaultIdentity<IdentityUser>()
-                    .AddRoles<IdentityUser>()
-                    .AddEntityFrameworkStores<ApplicationDbContext>()
-                    .AddDefaultTokenProviders();
-
-
-            // JWT
-            var appSettingsSection = Configuration.GetSection("AppSettings");
-            services.Configure<AppSettings>(appSettingsSection);
-
-            var appSettings = appSettingsSection.Get<AppSettings>();
-            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-
-
-
-            services.AddAuthentication(o =>
-           {
-               o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-               o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-           }).AddJwtBearer(b =>
-           {
-               b.RequireHttpsMetadata = true;
-               b.SaveToken = true;
-               b.TokenValidationParameters = new TokenValidationParameters
-               {
-                   ValidateIssuerSigningKey = true,
-                   IssuerSigningKey = new SymmetricSecurityKey(key),
-                   ValidateIssuer = true,
-                   ValidateAudience = true,
-                   ValidAudience = appSettings.ValidadoEm,
-                   ValidIssuer = appSettings.Emissor
-               };
-           });
-
             services.AddControllers();
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -75,8 +41,6 @@ namespace StoreGeek.Identidade.API
             app.UseRouting();
 
             app.UseAuthorization();
-
-            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
